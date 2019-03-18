0328bb3a3feeafa6a326323baf83d1f9da4fc649
sergiu.97 <thisis.sergiu@gmail.com>
2019-03-18 17:50:16 +0200

    asd

--- src/main/java/RomanSergiuVeleGeorge/App.java
+++ src/main/java/RomanSergiuVeleGeorge/App.java
@@ -1,13 +1,33 @@
 package RomanSergiuVeleGeorge;
 
+import Repository.NoteRepo;
+import Repository.StudentRepo;
+import Repository.TemeRepo;
+import Service.ServiceNote;
+import Service.ServiceStudent;
+import Service.ServiceTeme;
+import UI.UI;
+import Validator.NotaValidator;
+import Validator.StudentValidator;
+import Validator.TemeValidator;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
         System.out.println( "Hello World!" );
+        StudentRepo rep=new StudentRepo(new StudentValidator(),"D:\\vvss\\LabAssiAsseProjectV04\\src\\studenti.xml");
+        TemeRepo repo=new TemeRepo(new TemeValidator(),"D:\\vvss\\LabAssiAsseProjectV04\\src\\teme.xml");
+        NoteRepo r=new NoteRepo(new NotaValidator());
+        ServiceStudent srv=new ServiceStudent(rep);
+        ServiceTeme serv=new ServiceTeme(repo);
+        ServiceNote sv=new ServiceNote(r);
+        UI ui=new UI(srv,serv,sv);
+        ui.show();
     }
 }
